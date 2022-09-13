1922edf1be153933af97432278158d5dc01c8bc5
umnikos <alex.stefanov2@gmail.com>
2022-09-14 02:35:32 +0300

    implement writing to a file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
+
 fn main() {
-    let primes = make_primes(1_000_000_000);
+    let primes = make_primes(2_000_000_000);
+
+    //write_to_file(primes);
     println!("done.");
-    drop(primes);
+}
+
+fn write_to_file(primes : Vec<usize>) {
+    use std::fs::*;
+    use std::io::*;
+    let mut file = File::create("primes.txt").expect("can't make file!");
+    let mut out = String::new();
+    for p in primes {
+        let s = p.to_string();
+        let s = s + "\n";
+        out += &s;
+    }
+    file.write(out.as_bytes()).expect("can't write to file!");
+
 }
 
 fn make_primes(limit: usize) -> Vec<usize> {
@@ -15,7 +31,7 @@ fn make_primes(limit: usize) -> Vec<usize> {
         }
     }
 
-    let mut primes = Vec::new();
+    let mut primes = Vec::with_capacity(limit/2);
     for (i,&b) in sieve.iter().enumerate() {
         if b {
             primes.push(i+1);
