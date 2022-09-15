0597982854f22137d78127b33d79a0604686fa46
umnikos <alex.stefanov2@gmail.com>
2022-09-15 21:17:58 +0300

    BufWriter, Iterator, and extra skipping

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,47 @@
+use core::iter::*;
 
 fn main() {
-    let primes = make_primes(2_000_000_000);
+    let primes = make_primes(10_000_000_000);
 
     write_to_file(primes);
     println!("done.");
 }
 
-fn write_to_file(primes : Vec<usize>) {
+fn write_to_file<T: Iterator<Item=usize>>(primes: T) {
     use std::fs::*;
     use std::io::*;
-    let mut file = File::create("primes.txt").expect("can't make file!");
-    let mut out = String::new();
+
+    let file = File::create("primes.txt").expect("can't make file!");
+    let mut file = BufWriter::new(file); // BufWriter wrapper for performance
+
     for p in primes {
         let s = p.to_string();
         let s = s + "\n";
-        out += &s;
+        file.write(s.as_bytes()).expect("can't write to file!");
     }
-    file.write(out.as_bytes()).expect("can't write to file!");
 
 }
 
-fn make_primes(limit: usize) -> Vec<usize> {
-    let mut sieve = vec![true; limit];
+fn make_primes(limit: usize) -> impl Iterator<Item=usize> {
+    let mut sieve = vec![true; limit+1];
     sieve[0] = false;
-    for i in (2..limit).step_by(2) {
+    sieve[1] = false;
+    sieve[2] = true;
+    for i in (3..=limit).step_by(2) {
         if sieve[i] {
-            for j in (i+i+1..limit).step_by(i+1) {
+            for j in (i*i..=limit).step_by(i) {
                 sieve[j] = false;
             }
         }
     }
 
-    let mut primes = Vec::with_capacity(limit/2);
-    for (i,&b) in sieve.iter().enumerate() {
-        if b {
-            primes.push(i+1);
-        }
-    }
-    primes
+    let primes_without_2 =
+        sieve
+            .into_iter()
+            .enumerate()
+            .skip(1)
+            .step_by(2)
+            .filter_map(|(i, x)|
+                x.then_some(i));
+    std::iter::once(2).chain(primes_without_2)
 }
